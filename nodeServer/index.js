550988f3a78c509130981fce2cf59ac0597f03e8
Pranav <pranav.kumar@techchefz.com>
2021-03-30 22:37:30 +0530

    cors fix

---
+++ nodeServer/index.js
@@ -0,0 +1,26 @@
+//it will handle socket connection
+
+// const io = require("socket.io")(8000);
+
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "*",
+  },
+});
+
+//middleware
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
